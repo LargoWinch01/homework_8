f95563a9cb741cf75c962cdba097f022981697a2
LargoWinch01 <tokorobakidze7@gmail.com>
2024-09-10 14:20:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let colors = ['red','green','blue'];
 let firstColor = colors.shift();
 let lastColor = colors.pop(); 
 
-alert((`პირველი ფერი არის: ${firstColor}`));
-alert((`ბოლო ფერი არის: ${lastColor}`));
+alert(`პირველი ფერი არის: ${firstColor}`);
+alert(`ბოლო ფერი არის: ${lastColor}`);
 
 console.log(`პირველი ფერი არის: ${firstColor}`);
 console.log(`ბოლო ფერი არის: ${lastColor}`);
