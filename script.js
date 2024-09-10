d3e8d147af2b7dfd0bc8c203439fbfa2b3ac7f88
LargoWinch01 <tokorobakidze7@gmail.com>
2024-09-10 14:17:39 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let colors = ['red','green','blue'];
+
+let firstColor = colors.shift();
+let lastColor = colors.pop(); 
+
+alert((`პირველი ფერი არის: ${firstColor}`));
+alert((`ბოლო ფერი არის: ${lastColor}`));
+
+console.log(`პირველი ფერი არის: ${firstColor}`);
+console.log(`ბოლო ფერი არის: ${lastColor}`);
